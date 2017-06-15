fa64a16d069ea255e183eaa981f9b817ef94d37e
omidmehr <omidmehr69@gmail.com>
2017-06-15 16:02:54 +0430

    edit hook

--- hook.php
+++ hook.php
@@ -1,4 +1,9 @@
 <?php
+ini_set('error_reporting', E_ALL);
+use \Longman\TelegramBot\Request;
+use \Longman\TelegramBot\Telegram;
+use Longman\TelegramBot\Exception\TelegramException;
+
 // Load composer
 require __DIR__ . '/vendor/autoload.php';
 
@@ -6,17 +11,17 @@ $API_KEY = '354937959:AAGorUCSKKU0uQNeLEkDUkkQROnlZ_yM6hs';
 $BOT_NAME = 'bookingvilla_bot';
 try {
     // Create Telegram API object
-    $telegram = new Longman\TelegramBot\Telegram($API_KEY, $BOT_NAME);
+    $telegram = new Telegram($API_KEY, $BOT_NAME);
     $res = file_get_contents("php://input");
-    $resArr = json_decode($res);
+    $resArr = json_decode($res,true);
     var_dump($telegram);
     echo '<br>';
 var_dump($resArr);
-    $chat_id = $resArr['result'][0]['message']['chat']['id'];
-    echo $chat_id;
-    \Longman\TelegramBot\Request::sendMessage(['chat_id' => $chat_id, 'text' => 'Your utf8 text ًںکœ ...']);
+//    $chat_id = $resArr['result'][0]['message']['chat']['id'];
+//    echo $chat_id;
+//    Request::sendMessage(['chat_id' => $chat_id, 'text' => 'Your utf8 text ًںکœ ...']);
 
-} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+} catch (TelegramException $e) {
     // Silence is golden!
     // log telegram errors
     echo $e;
